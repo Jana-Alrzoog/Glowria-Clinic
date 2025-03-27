cd58a8789327fb93ef39b86f2fc8a7394f4e505f
Nouf <169012640+Nuv09@users.noreply.github.com>
2025-03-27 21:08:00 +0300

    Database for testing

---
+++ Phase 2/glowria_clinic_database.sql
@@ -0,0 +1,278 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 27, 2025 at 06:07 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `glowria_clinic_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `PatientID` int(11) DEFAULT NULL,
+  `DoctorID` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `reason` varchar(255) DEFAULT NULL,
+  `status` enum('Pending','Confirmed','Done') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `PatientID`, `DoctorID`, `date`, `time`, `reason`, `status`) VALUES
+(1, 1, 10, '2025-04-01', '10:00:00', 'Lip Fillers Consultation', 'Confirmed'),
+(2, 2, 10, '2025-04-02', '11:30:00', 'Back pain', 'Confirmed'),
+(3, 3, 10, '2025-04-03', '09:00:00', 'General checkup', 'Done'),
+(4, 1, 10, '2025-03-26', '11:47:31', 'pain', 'Confirmed'),
+(5, 2, 10, '2025-03-18', '11:47:31', 'test', 'Confirmed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `uniqueFileName` varchar(255) DEFAULT NULL,
+  `SpecialityID` int(11) DEFAULT NULL,
+  `emailAddress` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `firstName`, `lastName`, `uniqueFileName`, `SpecialityID`, `emailAddress`, `password`) VALUES
+(1, 'Layla', 'Hassan', 'aisha123.jpg', 1, 'aisha@example.com', 'hashed_password_4'),
+(2, 'Omar', 'Yusuf', 'omar123.jpg', 2, 'omar@example.com', 'hashed_password_5'),
+(3, 'Nada', 'Fahad', 'nada123.jpg', 3, 'nada@example.com', 'hashed_password_6'),
+(10, 'TestFirst6', 'TestLast6', '67dd0cffb93dc.jpg', 3, 'test6@example.com', '$2y$10$1f0VmfTZxe2MFvt70SPAzOskaavEksGue5tWigS/Va6kDO7f8StT.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medication`
+--
+
+CREATE TABLE `medication` (
+  `id` int(11) NOT NULL,
+  `MedicationName` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `medication`
+--
+
+INSERT INTO `medication` (`id`, `MedicationName`) VALUES
+(1, 'Paracetamol'),
+(2, 'Ibuprofen'),
+(3, 'Amoxicillin'),
+(4, 'Vitamin D'),
+(5, 'Aspirin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `gender` enum('Male','Female') DEFAULT NULL,
+  `DoB` date DEFAULT NULL,
+  `emailAddress` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `firstName`, `lastName`, `gender`, `DoB`, `emailAddress`, `password`) VALUES
+(1, 'Leena', 'Naser', 'Female', '1985-06-15', 'leena@example.com', 'hashed_password_1'),
+(2, 'Majed', 'Saleh', 'Male', '1990-09-10', 'majed@example.com', 'hashed_password_2'),
+(3, 'Sara', 'Ali', 'Female', '1995-02-20', 'sara@example.com', 'hashed_password_3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `id` int(11) NOT NULL,
+  `AppointmentID` int(11) DEFAULT NULL,
+  `MedicationID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `prescription`
+--
+
+INSERT INTO `prescription` (`id`, `AppointmentID`, `MedicationID`) VALUES
+(1, 3, 4),
+(2, 3, 3),
+(3, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `speciality`
+--
+
+CREATE TABLE `speciality` (
+  `id` int(11) NOT NULL,
+  `speciality` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `speciality`
+--
+
+INSERT INTO `speciality` (`id`, `speciality`) VALUES
+(1, 'Aesthetic Skincare'),
+(2, 'Plastic Surgery'),
+(3, 'Orthopedics'),
+(4, 'Cosmetic Surgery');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `PatientID` (`PatientID`),
+  ADD KEY `DoctorID` (`DoctorID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `SpecialityID` (`SpecialityID`);
+
+--
+-- Indexes for table `medication`
+--
+ALTER TABLE `medication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `AppointmentID` (`AppointmentID`),
+  ADD KEY `MedicationID` (`MedicationID`);
+
+--
+-- Indexes for table `speciality`
+--
+ALTER TABLE `speciality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `medication`
+--
+ALTER TABLE `medication`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `speciality`
+--
+ALTER TABLE `speciality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`PatientID`) REFERENCES `patient` (`id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`id`);
+
+--
+-- Constraints for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`SpecialityID`) REFERENCES `speciality` (`id`);
+
+--
+-- Constraints for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`AppointmentID`) REFERENCES `appointment` (`id`),
+  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`MedicationID`) REFERENCES `medication` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
